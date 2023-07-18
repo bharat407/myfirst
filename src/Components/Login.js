3e738c4a52da32cfdd93be310a5fec8ee3c7ba5f
Bharat Kushwah <bharatkushwah407@gmail.com>
2023-07-18 08:39:05 +0530

    update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -47,12 +47,6 @@ const Login = () => {
 
           <div className="block mt-4">
             <label className="inline-flex items-center">
-              {/* <input
-                id="remember"
-                type="checkbox"
-                className=" appearance-none w-4 h-4 border border-gray-300 rounded checked:bg-blue-600 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                name="remember"
-              /> */}
               <input
                 type="checkbox"
                 id="remember"
